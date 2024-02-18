e5b73e3957efe1282c17993d647192da212b4c0a
Ilteris Kaqan Colak <ilterisskaanncolakk@gmail.com>
2024-02-18 17:12:26 +0300

    vecihleSystem

---
+++ src/Car.java
@@ -0,0 +1,77 @@
+public class Car extends Vecihle{
+
+    private int wheels;
+    private String type;
+    private int doors;
+    private int gears;
+    private boolean isManuel;
+    private int currentGear;
+
+    public Car(String name, int wheels, int doors, int gears, boolean isManuel, String type) {
+        super(name);
+        this.type = type;
+        this.gears = gears;
+        this.doors = doors;
+        this.isManuel = isManuel;
+        this.wheels = wheels;
+        currentGear = 1;
+    }
+
+    public void changeGear(int newGear) {
+        this.currentGear = newGear;
+        System.out.println("changeGear method called: Changed to "+ this.currentGear + "gear ");
+    }
+
+    public void changeSpeed(int newSpeed, int newDirection) {
+        move(newSpeed, newDirection);
+        System.out.println("Change speed method called: Speed -> "+ newSpeed+" Direction -> "+ newDirection);
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public boolean isManuel() {
+        return isManuel;
+    }
+
+    public void setManuel(boolean manuel) {
+        isManuel = manuel;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+     public static void main(String[] args) {
+
+         SUV Ford = new SUV("Ranger",false);
+         Ford.move(40,0);
+         Ford.accelerate(20);
+         Ford.accelerate(-60);
+         System.out.println("Current gear :" + Ford.getCurrentGear());
+         System.out.println("Current gear :" + Ford.getCurrentSpeed());
+    }
+}
